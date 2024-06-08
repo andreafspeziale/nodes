865a2374f8be2268073562a6c68f04a178a5cb88
andreafspeziale <andreaf.speziale@gmail.com>
2024-06-08 23:15:05 +0200

    feat: add python-semantic-release

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 
+__version__ = "0.0.0"
+
 setup(
     name="nodes",
-    version="1.0",
+    version=__version__,
     packages=find_packages(),
     install_requires=[
         "flask==3.0.3",
         "webargs==8.4.0",
     ],
     extras_require={
-        "dev": ["mypy==1.10.0", "pytest==8.2.1", "black==24.4.2", "pylint==3.2.2"],
+        "dev": [
+            "mypy==1.10.0",
+            "pytest==8.2.1",
+            "black==24.4.2",
+            "pylint==3.2.2",
+            "python-semantic-release==9.8.1",
+        ],
     },
 )
